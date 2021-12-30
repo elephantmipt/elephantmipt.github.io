86885ac89b57c2f1b3c5b38ac8f51851c3f94e1c
Балаганский Никита Николаевич <n.n.balaganskiy@tinkoff.ru>
2021-12-30 16:33:10 +0300

    fix

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:elephanmipt/elephanmipt.github.io.git master:gh-pages
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:wowthemesnet/vuepress-theme-mediumish.git master:gh-pages
+
+cd -
